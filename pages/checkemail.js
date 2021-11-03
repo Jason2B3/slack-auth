bb09acf408fe7da95892cf6de2db6cd9fce2e849
jason <2B3>
2021-11-03 12:23:01 -0400

    npm installs and NextAuth setup

--- pages/checkemail.js
+++ pages/checkemail.js
@@ -2,6 +2,7 @@ import React from "react";
 import CheckEmail1 from "../components/auth/CheckEmail1";
 
 function checkemail() {
+  console.log(process.env.NEXT_PUBLIC_ALIAS)
   return <CheckEmail1 />;
 }
 
---
+++ pages/secret.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { getSession } from "next-auth/client";
+
+export default function UserProfile() {
+  // Create/manage your own loading and session states
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedSession, setLoadedSession] = useState(null);
+  console.log(isLoading, loadedSession);
+
+  useEffect(() => {
+    const confirmSession = async function () {
+      const session = await getSession(); // falsy if we aren't logged in
+      setLoadedSession(session); // update our session state
+      // If getSession returns a falsy, we must not be logged in
+      // In this case, redirect the visitor to to the /auth path for authentication
+      // WARNING: this method of redirecting resets our app (all state lost)
+      if (!session) alert("You are not logged in!");
+      else setIsLoading(false); // update our loading state (we're done now)
+    };
+    confirmSession();
+  }, []);
+
+  if (isLoading) return <p className={classes.profile}>Loading...</p>;
+  return (
+    <section className={classes.profile}>
+      <h1>We are currently {loadedSession && "logged in"}</h1>
+    </section>
+  );
+}
