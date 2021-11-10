36c03b17a39d30e7724115f13160bedf4a1c4de9
jason <2B3>
2021-11-09 23:36:13 -0500

    failed attempt at SendGrid

--- pages/api/auth/credentials/sendVerificationEmail.js
+++ pages/api/auth/credentials/sendVerificationEmail.js
@@ -1,5 +1,6 @@
 import { connectToDatabase } from "../../../../helpers/db";
 import { hashPW } from "../../../../helpers/auth";
+import { sendEmail } from "../../../../helpers/sendEmail";
 import * as securePin from "secure-pin";
 
 export default async function handler(req, res) {
@@ -21,36 +22,22 @@ export default async function handler(req, res) {
     // Create a 6 digit PIN and hash it
     var charSet = new securePin.CharSet();
     charSet.addUpperCaseAlpha().addNumeric().randomize();
-    const PIN = await securePin.generateStringSync(6, charSet);
+    const PIN = securePin.generateStringSync(6, charSet);
     const hashedPIN = await hashPW(PIN);
 
     //% Send an email to the submitted address with the unhashed PIN
     const emailData = {
-      service_id: process.env.EMAILJS_SERVICE_ID,
-      template_id: process.env.EMAILJS_TEMPLATE_ID,
-      user_id: process.env.EMAILJS_USER_ID,
-      template_params: {
-        message: PIN, // pin will be a part of the email
-        target: email, // address to send email to
-        reply_to: "no one", // a param we need for the email (See template)
-      },
+      targetEmail: email,
+      subject: "Finish signing up to slack-auth",
+      message: `Visit slack-auth/verifyEmail and enter in the following 6 digit code to finish the sign in process. ${String(PIN)}`,
     };
-    const sendEmailReq = await fetch(
-      "https://api.emailjs.com/api/v1.0/email/send",
-      {
-        method: "POST",
-        body: JSON.stringify(emailData),
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-    // const emailAPIResponse = await JSON.parse(sendEmailReq);
-    console.log(sendEmailReq);
-    // if(sendEmailReq){
-
-    // }
+    const sendEmailReq = await sendEmail(emailData);
+    //% ----------------------------------------------
     // Return hashed PIN to the front end so you can store it in auth-context.js
     client.close();
-    res.status(201).json({ message: "Sent a verification email", hashedPIN, sendEmailReq });
+    res
+      .status(201)
+      .json({ message: "Sent a verification email", hashedPIN, sendEmailReq });
   }
 }
 //! WARNING: No error handling here yet
